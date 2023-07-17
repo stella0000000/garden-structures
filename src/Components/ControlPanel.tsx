3bcef497cc883747ca12bc57a671487c7e66330a
stella <stella.choi@hyperisland.se>
2023-07-16 23:17:05 -0400

    fix mobile

--- src/Components/ControlPanel.tsx
+++ src/Components/ControlPanel.tsx
@@ -40,7 +40,7 @@ const PositionWrap = styled.div`
   /* border: 1px solid red; */
 
   @media only screen and (max-width: 700px) {
-    margin: -50px 10px;
+    /* margin: -50px 10px; */
     width: 150px;
   }
 `;
